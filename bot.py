4b350dd2da4391901167b35bf20e2f4aa89f12f2
odutt4440-cmyk <odutt4440@gmail.com>
2026-08-30 21:53:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
-LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID", "")
+LOG_CHANNEL_ID = "bwuahahahahaa"
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 UPI_ID_TEXT = os.getenv("UPI_ID_TEXT", "yourupi@bank")
